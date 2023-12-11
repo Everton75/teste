5af294ed6cec66a531a9bddd54b8cbd27821e370
Everton_Almeida <evertonexpert75@gmail.com>
2023-12-11 08:12:49 -0300

    login logout funcionado

--- codigo_certo.py
+++ codigo_certo.py
@@ -8,7 +8,8 @@ app.secret_key = 'sua_chave_secreta'
 # Configuração do banco de dados SQLite3
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)')
+cursor.execute(
+    'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)')
 conn.commit()
 
 # Configuração do Flask Login
@@ -16,12 +17,16 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 # Classe de usuário
+
+
 class User(UserMixin):
     def __init__(self, id, username):
         self.id = id
         self.username = username
 
 # Função para carregar o usuário
+
+
 @login_manager.user_loader
 def load_user(user_id):
     conn = sqlite3.connect('database.db')
@@ -33,6 +38,8 @@ def load_user(user_id):
     return None
 
 # Rota de login
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -40,22 +47,28 @@ def login():
         password = request.form['password']
         conn = sqlite3.connect('database.db')
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+        cursor.execute(
+            'SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
         user = cursor.fetchone()
         if user:
             user_obj = User(user[0], user[1])
             login_user(user_obj)
             return redirect(url_for('logado'))
-    return render_template('login.html')
+
+    return redirect(url_for('index'))
 
 # Rota de logout
+
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index.html'))
+    return redirect(url_for('index'))
 
 # Rota protegida
+
+
 @app.route('/logado')
 @login_required
 def logado():
@@ -65,6 +78,7 @@ def logado():
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
